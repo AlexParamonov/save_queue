4ada4cbc7ef3f00cfaa5c4823e735448d4da5e13
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2012-01-23 17:13:29 +0200

    fix for ruby 1.9.2

--- lib/save_queue/plugins/notification/object.rb
+++ lib/save_queue/plugins/notification/object.rb
@@ -2,17 +2,28 @@ module SaveQueue
   module Plugins
     module Notification
       module Object
-        def self.included base
-          queue_creator = Module.new do
-            def create_queue
-              super
-              queue = instance_variable_get("@_save_queue")
-              raise "save queue should respond to add_observer in order to work correctly" unless queue.respond_to? :add_observer
-              queue.add_observer(self, :queue_changed_event)
-            end
+        module AddObserverToQueue
+          def create_queue
+            super
+            queue = instance_variable_get("@_save_queue")
+            raise "save queue should respond to add_observer in order to work correctly" unless queue.respond_to? :add_observer
+            queue.add_observer(self, :queue_changed_event)
           end
+        end
+        
+        def self.included base
 
-          base.send :include, queue_creator
+          #queue_creator = Module.new do
+          #  def create_queue
+          #    super
+          #    queue = instance_variable_get("@_save_queue")
+          #    raise "save queue should respond to add_observer in order to work correctly" unless queue.respond_to? :add_observer
+          #    queue.add_observer(self, :queue_changed_event)
+          #  end
+          #end
+
+          #base.send :include, queue_creator
+          base.send :include, AddObserverToQueue unless base.include?(AddObserverToQueue)
         end
 
         def queue_changed_event(result, object)
