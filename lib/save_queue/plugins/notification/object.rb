4dc947b91d3043614fc856d4865e24429107581f
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2012-02-02 14:42:01 +0200

    Remove unnecessary module from Notification Object

--- lib/save_queue/plugins/notification/object.rb
+++ lib/save_queue/plugins/notification/object.rb
@@ -2,22 +2,14 @@ module SaveQueue
   module Plugins
     module Notification
       module Object
-        def self.included(base)
-          base.send :include, AddObserverToQueue
-        end
+        def queue_changed_event(result, object, *args); end
 
-        def queue_changed_event(result, object, *args)
+        def create_queue
+          super
 
-        end
-
-        module AddObserverToQueue
-          def create_queue
-            super
-
-            queue = instance_variable_get("@_save_queue")
-            raise "save queue should respond to add_observer in order to work correctly" unless queue.respond_to? :add_observer
-            queue.add_observer(self, :queue_changed_event)
-          end
+          queue = instance_variable_get("@_save_queue")
+          raise "save queue should respond to add_observer in order to work correctly" unless queue.respond_to? :add_observer
+          queue.add_observer(self, :queue_changed_event)
         end
       end
     end
